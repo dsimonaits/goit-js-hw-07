eed119a4cbf58ca7968f39ed73fa2113fa5fbfba
Deniss Simonaits <dsimonaits@gmail.com>
2022-09-15 22:18:58 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const createGalleryItem = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("afterbegin", createGalleryItem);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>;
+    `;
+    })
+    .join("");
+}
+
+console.log(createGalleryItem);
