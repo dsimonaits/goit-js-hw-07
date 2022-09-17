8201ff7e1bc058b16551f51c2e6c2e7c72657782
Deniss Simonaits <dsimonaits@gmail.com>
2022-09-17 18:49:19 +0300

    Task start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const createGalleryItem = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML("afterbegin", createGalleryItem);
+
+galleryRef.addEventListener("click", onGalleryItemClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" data-href="${original}" target="_blank">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>`;
+    })
+    .join("");
+}
+
+function onGalleryItemClick(e) {
+  const galleryItem = e.target.classList.contains("gallery__image");
+  if (!galleryItem) {
+    return;
+  }
+
+  const itemSource = e.target.dataset.source;
+
+  createLigthBoxItem(itemSource);
+}
