71e77ef1706b2cb55bd6c4373386b1b1c0496978
Deniss Simonaits <dsimonaits@gmail.com>
2022-10-29 21:00:32 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -16,8 +16,9 @@ function createGalleryMarkup(galleryItems) {
     })
     .join("");
 }
-
-const lightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250,
-});
+setTimeout(() => {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}, 500);
